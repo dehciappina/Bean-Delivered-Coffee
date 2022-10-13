95fccfde95e7912af29a978d07b1a0535b977a88
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2022-10-13 17:50:44 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+console.log('done')
+
+
+
+
+
+
+
+
+
+
+/* LOADING BG + DOCUMENT.READYSTATE */
+
+/* const loadingBg = document.getElementById('loading_bg')
+
+document.onreadystatechange = function () {
+    if (document.readyState == "interactive" || document.readyState == "complete") {
+        loadingBg.style.opacity = 0;
+        loadingBg.style.visibility = 'hidden';
+        setTimeout(() => {
+            menuBt.style.transform = 'translateY(0) rotate(0deg)'
+        }, 200);
+    }
+} */
+
+
+
+
+
+/* HAMBURGUER MENU */
+
+/* const hambt = document.querySelector('.hambt')
+const nav = document.querySelector('header nav')
+
+const hamDivs = document.querySelectorAll('.hambt div')
+
+let showingNav = false;
+
+function toggleNav() {
+    if(showingNav == false) {
+        showingNav = true;
+        nav.classList.add('showing_nav')
+
+        for(i=0;i<hamDivs.length;i++) {
+            hamDivs[i].style.backgroundColor = 'var(--lighta)';
+        }
+    } else {
+        showingNav = false;
+        nav.classList.remove('showing_nav')
+
+        for(i=0;i<hamDivs.length;i++) {
+            hamDivs[i].style.backgroundColor = 'var(--c3)';
+        }
+    }
+}
+
+hambt.addEventListener('click', toggleNav)
+
+document.body.onscroll = function() {
+    if(showingNav == true) {
+        toggleNav()
+    }
+} */
